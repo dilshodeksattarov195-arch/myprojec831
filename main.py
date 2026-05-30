7ecd1caacc143b176f39db1bfd5e1d62c99b9991
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 10:34:55 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class smsDalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = smsDalculateWorker()
+    print("Worker engine initialized.")
